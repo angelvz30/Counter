4076a1c9dd332a901ac79ed383ab65d8eb3ceaeb
angelvz30 <angelvaz3510@gmail.com>
2025-01-06 11:06:08 -0800

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by QHS on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
