f4bd0070a4140c2925bf8e7c01058e3f3c640b79
angelvz30 <angelvaz3510@gmail.com>
2025-01-13 11:21:26 -0800

    Increment and reset button

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -8,19 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Counter: Int = 0
+    @State var counterDisplay: String = "0"
+    @State var showingReset = false
     var body: some View {
-        VStack {
-            Text("Counter")
-                .font(.largeTitle)
-            
-            Button("Increment")
-             Counter += 1
+        NavigationStack{
+            VStack {
                 
-            }
+                Text(counterDisplay)
+                    .font(.largeTitle)
+                
+                Button("Increment") {
+                    Counter += 1
+                    counterDisplay = String(Counter)
+                    
+                }
+                Button("Reset") {
+                    
+                }
             }
         }
-    
-
+    }
+}
 #Preview {
     ContentView()
 }
---
+++ Counter/resetView.swift
@@ -0,0 +1,18 @@
+//
+//  resetView.swift
+//  Counter
+//
+//  Created by QHS on 1/13/25.
+//
+
+import SwiftUI
+
+struct resetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    resetView()
+}
