977cd3e05e45a1983c8c685e2ee2139cb010ce3d
angelvz30 <angelvaz3510@gmail.com>
2025-01-28 10:08:53 -0800

    fixed the confirm reset button, binding variables, fixed brace and spacing errors.

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var Counter: Int = 0 //current count
+    @State var counter: Int = 0 //current count
     @State var counterDisplay: String = "0" //Display counter
     @State var showingReset = false
     @State var confirmReset = false
     var body: some View { //entire view
-        NavigationStack{ //Navigation stack view
+        NavigationStack { //Navigation stack view
             VStack { //VStack view
                 Text(counterDisplay)
                     .font(.largeTitle)
                 Button("Increment") { //Counter button
-                    Counter += 1
-                    counterDisplay = String(Counter)
+                    counter += 1
+                    counterDisplay = String(counter)
                 } // Close counter button
                 Button("Reset") { //Reset counter button
                     showingReset = true
                 } //Close reset counter button
             } //Close VStack view
                 .navigationDestination(isPresented: $showingReset) { //Show reset view
-                    resetView()
+                    resetView(counter: $counter, counterDisplay: $counterDisplay)
                 } //Close show reset view
         } //Close navigation stack view
     } //close View
--- Counter/resetView.swift
+++ Counter/resetView.swift
@@ -8,16 +8,26 @@
 import SwiftUI
 
 struct resetView: View {
+    @Environment(\.dismiss) var dismiss
+    @Binding var counter: Int //current count
+    @Binding var counterDisplay: String //Display counter
     var body: some View {
         VStack {
             Button("Confirm reset") {
-                confirmReset = true
-                
+                counterDisplay = "0"
+                counter = 0
+                dismiss()
             }
         }
     }
 }
-
 #Preview {
-    resetView()
+    struct Preview: View {
+        @State var count = 0
+        @State var stringCount = "0"
+        var body: some View {
+            resetView(counter: $count, counterDisplay: $stringCount)
+        }
+    }
+    return Preview()
 }
